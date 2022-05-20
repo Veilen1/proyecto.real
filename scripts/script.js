2a59e48c280c68b40d3b7d8631b43dd9b48e2a02
Veilen1 <devrientv@gmail.com>
2022-05-20 01:26:21 -0300

    Agregando boton saber más, falta corregir

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let books = [
         genero : "Saga compuesta por (Más allá de la realidad (I)- Histeria- El escuadrón de los Marginados- Una canción para Alex (II) )",
         id : 1,
         img : "https://img.wattpad.com/cover/56920419-200-k39815.jpg",
+        pag : "https://www.wattpad.com/story/56920419-la-relación-de-la-triada-mas-allá-de-la-realidad",
         descrip : "La triada es una saga Young Adult de tres historias divididas en cuatro libros. Pueden leerse de manera independiente o en siguiendo el orden propuesto (Más allá de la realidad (I)- Histeria- El escuadrón de los Marginados- Una canción para Alex (II) ). Hacia el ﬁnal, el lector descubre que todas las historias están relacionadas./n En las cuatro novelas hablamos de personajes rotos, incomprendidos, diferentes. Es a través de éstas tres bases que logran subsistir en el mundo: En Más allá de la realidad y Una canción para Alex el amor, en Histeria el fanatismo y en en El escuadrón de los marginados, la amistad.",
     },
     {
@@ -11,6 +12,7 @@ let books = [
         genero : "Romance young adult",
         id : 2,
         img : "https://img.wattpad.com/cover/35218331-200-k559526.jpg",
+        pag : "https://www.wattpad.com/story/35218331-mas-allá-de-la-realidad",
         descrip : "Alexandra Goodman comienza su último primer día en el secundario contando los minutos para que termine. Su pasión es el arte y dado que no tiene verdaderos amigos, pasa las horas leyendo, pintando y escuchando música en su habitación. En el pasillo del secundario conoce a JamesMc Owen, un estudiante de intercambio que escapa de un pasado traumático en secreto. La atracción entre ambos es instantánea y rápidamente comienzan un apasionado romance hasta que una terrible noticia amenaza con destruir lo que recién descubren.",
     },
     {
@@ -18,6 +20,7 @@ let books = [
         genero : "Coming-of-age",
         id : 3,
         img : "https://img.wattpad.com/cover/41908908-200-k559627.jpg",
+        pag : "https://www.wattpad.com/story/41908908-histeria",
         descrip : "El mundo surrealista de una aspirante a escritora adolescente y fanática hasta los huesos, estrella en una intensa aventura de una noche con el cantante principal de su banda indie-rock favorita. A la fuerza, ella debe confrontar y deshacerse de su falsa idealización y las demandas del fanatismo mientras atraviesa decepciones en la amistad, trata de arreglar a su familia rota y elegir su propio futuro como cualquier otra chica de su edad.",
     },
     {
@@ -25,6 +28,7 @@ let books = [
         genero : "Drama young adult",
         id : 4,
         img : "https://img.wattpad.com/cover/51884271-512-k56448.jpg",
+        pag : "https://www.wattpad.com/story/51884271-el-escuadrón-de-los-marginados-tos",
         descrip : "Algo malo le pasó al geek Tyler Dobson pero es su permanente y profunda obsesión con la revoltosa pelirroja Margeary White lo que lo hace olvidarse de ello. Después de un slam de poesía, Tyler conoce a los amigos de Margeary, un escuadrón de marginados que sufrió lo suficiente como para dejar que sus pasados guíen sus futuros y juntos pueden llegar a descubrir que el poder de la amistad reside en pequeños actos donde quepa la libertad pero siempre detrás de una conversación honesta. ",
     },
     {
@@ -32,6 +36,7 @@ let books = [
         genero : "Romance epistolar young adult",
         id : 5,
         img : "https://img.wattpad.com/cover/63905515-200-k248371.jpg",
+        pag : "https://www.wattpad.com/story/63905515-una-canción-para-alex-ii-libro-de-más-allá-de-la",
         descrip : "Todo parece perdido y la oscuridad persigue a James Mc Owen después de la sorpresiva muerte de su novia Alexandra. Pero una vieja amiga lo sorprende con una caja llena de cartas que Alexandra escribió para él, e instrucciones para que sean abiertas a lo largo de los años y en ocasiones especiales. ¿Es esta la mejor idea que alguien haya tenido jamás o la mayor tortura que pueda existir? A medida que abre las cartas, James emprende un emotivo viaje que durará el resto de su vida, mientras responde esas cartas a alguien que continúa viviendo en u memoria y en su corazón y a quién siente cada vez más viva.",
     },
     {
@@ -39,6 +44,7 @@ let books = [
         genero : "Queer new adult",
         id : 6,
         img : "https://img.wattpad.com/cover/149012101-512-k412231.jpg",
+        pag : "https://www.wattpad.com/story/149012101-s-he-la-chica-y-la-generación-espontánea",
         descrip : "Después de ganar una beca de seis meses en fotografía, Skyler, una chica introvertida de diecinueve años, se muda a Quebec desde su pequeño pueblo para ir a la universidad. Deja a Adam, su novio de toda la vida en su pueblo y una vez en la ciudad conoce a Jesse, una magnética andrógina estudiante de cine y su extrovertido, mejor amigo gay, Quentin. El trío se saltea clases para viajar en auto por el sur de Canadá y es en este mismo viaje donde Skyler y Jesse, se enamoran. Pero pronto, Skyler debe descubrir que es lo que quiere y quien es ella en verdad o podría terminar lastimando a todos, sobre todo cuando Adam viene a Quebec como sorpresa. ",
     },
     {
@@ -46,6 +52,7 @@ let books = [
         genero : "Ciencia ficción post-apocalíptica y distópica",
         id : 7,
         img : "https://img.wattpad.com/cover/117286962-512-k963262.jpg",
+        pag : "https://www.wattpad.com/story/117286962-arcadia-etapa-i-evolución",
         descrip : "Asis despierta en una estación de investigación cerebral sin ser capaz de recordar nada acerca de su vida, ni siquiera su propio nombre. De a poco e invadido por la rutina de bienvenida hacia los letrados de esta nueva división, Asis descubre que es un eslabón clave para la ejecución de Arcadia, un programa de sueño inseminado. Asis, Iwin, Enoch y Ray deberán emprender un viaje para salirse de los bordes de Arcadia enfrentando sus propios miedos en cada uno de sus inconscientes para al final recibir el mensaje que de un sueño solo se vuelve muriendo; ahora y en medio de un peligro inminente, deben decidir en quién creer.",
     },
     {
@@ -53,6 +60,7 @@ let books = [
         genero : "No ficción, Poesía ilustrada",
         id : 8,
         img : "https://img.wattpad.com/cover/195659918-512-k402242.jpg",
+        pag : "https://www.wattpad.com/story/195659918-empecemos-por-decir-adiós",
         descrip : "",
     },
     {
@@ -60,6 +68,7 @@ let books = [
         genero : "No ficción, Cuentos y relatos cortos",
         id : 9,
         img : "https://img.wattpad.com/cover/69177055-512-k545679.jpg",
+        pag : "https://www.wattpad.com/story/69177055-manifiesto-absurdo-de-un-presente-que-no-existe",
         descrip : "",
     },
     {
@@ -67,6 +76,7 @@ let books = [
         genero : "No ficción",
         id : 10,
         img : "https://img.wattpad.com/cover/165472465-512-k408313.jpg",
+        pag : "https://www.wattpad.com/story/165472465-feminista",
         descrip : "",
     },
 ]
@@ -77,17 +87,19 @@ for (let libro of books) {
     const div = document.createElement("div")
 
     div.innerHTML = ` 
-<div class="card mb-3" style="max-width: 1200px; min-width: 865px;">
-<div class="row g-0">
+<div class="card mb-5 " style="max-width: 1200px; min-width: 865px;">
+<div class="row g-0 ">
     <div class="col-md-4">
+    <a target="_blank" href="${libro.pag}">
         <img src="${libro.img}" class="img-fluid rounded-start imgCards" alt="${libro.nombre}.img">
+    </a>
     </div>
     <div class="col-md-8">
         <div class="card-body">
-            <h5 class="card-title h1">${libro.nombre}</h5>
-            <p class="card-text h2">${libro.genero}</p>
-            <p class="card-text h4">${libro.descrip}</p>
-            <p class="card-text h5"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title tituloCard ">${libro.nombre}</h5>
+            <p class="card-text h4 m-4">${libro.genero}</p>
+            <p class="card-text h5">${libro.descrip}</p>
+            <p class="card-text h5"><small class="text-muted"></small></p>
         </div>
     </div>
 </div>
@@ -96,8 +108,22 @@ for (let libro of books) {
     contenedor.append(div)
 
 }
-
-const eventoNav = document.getElementById("eventoNav");
-eventoNav.addEventListener ("scroll", () => {
-
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY>0)
 })
+const contenedor2 =document.getElementById("contenedor2");
+const btnBio = document.getElementById("btnBio")
+
+function crearBtnBio () {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class=""><h1>lorem</h1></div>
+        `;
+    contenedor2.append(div)
+}
+btnBio.addEventListener("click", () => {
+    crearBtnBio();
+})
+
+
