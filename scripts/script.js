dc53c45234594715f0f6a1d9db452dbc797dab9c
Veilen1 <devrientv@gmail.com>
2022-05-21 05:17:53 -0300

    Página Terminada

--- scripts/script.js
+++ scripts/script.js
@@ -87,7 +87,7 @@ for (let libro of books) {
     const div = document.createElement("div")
 
     div.innerHTML = ` 
-<div class="card mb-5 " style="max-width: 1200px; min-width: 865px;">
+<div class="card mb-5 eos-init" style="max-width: 1200px; min-width: 865px;" data-aos="zoom-in" data-aos-delay="175" data-aos-duration="1500">
 <div class="row g-0 ">
     <div class="col-md-4">
     <a target="_blank" href="${libro.pag}">
@@ -95,8 +95,8 @@ for (let libro of books) {
     </a>
     </div>
     <div class="col-md-8">
-        <div class="card-body">
-            <h5 class="card-title tituloCard ">${libro.nombre}</h5>
+        <div class="card-body letraAltona">
+            <h5 class="card-title tituloCard " style="font-width:"1000";">${libro.nombre}</h5>
             <p class="card-text h4 m-4">${libro.genero}</p>
             <p class="card-text h5">${libro.descrip}</p>
             <p class="card-text h5"><small class="text-muted"></small></p>
@@ -116,14 +116,35 @@ const contenedor2 =document.getElementById("contenedor2");
 const btnBio = document.getElementById("btnBio")
 
 function crearBtnBio () {
-    const div = document.createElement("div");
+    
+    let div = document.createElement("div");
     div.innerHTML = `
-        <div class=""><h1>lorem</h1></div>
+    <div class="d-flex justify-content-center">
+        <div class="card-bio p-4 m-4" data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000">
+        <h1 class="h4 letraAltona" style="color: "white"; line-height: 30%; font-size:35px;">${biografia}</h1>
+        </div>
+    </div>
         `;
     contenedor2.append(div)
 }
 btnBio.addEventListener("click", () => {
-    crearBtnBio();
+    let i = 0;
+    if (i === 0){
+        crearBtnBio();
+        window.scrollTo(0, 6255);
+        i++;
+    } else {
+        window.scrollTo(0, 6255);
+    }
 })
 
+window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY;
+
+    console.log(scrolled);
+})
+
+const biografia = "Nací incómoda un 5 de marzo de 1995, en un contexto familiar y escolar extremadamente religioso que me ayudó a sentar las bases de mi propia espiritualidad: la creencia en mi misma y la intolerancia a vivir y compartir con gente que no habita la empatía. De niña viví un tiempo en Houston, Texas por los estudios de mi papá. Mi mamá me crió poniendome películas VHS en ingles, gestándose en mí, una fascinación temprana por el cine y lo bilingüe. Tenía 12 años cuando comencé a escribir novelas - Los diálogos y las tramas se me solían ocurrir en inglés; luego los traducía al español en mi mente, pero mis historias juveniles, seguramente por influencia globalizada yankee, siempre ocurrieron en lugares de habla inglesa. Me frustraba mucho no poder justificarlo, después crecí. Comencé a trabajar a mis quince años dando clases de guitarra, luego babysitter y más adelante, recepcionista. Cuando cumplí 18, en medio de la expansión de internet, enfrenté mi primer desafío: ¿iba a esperar que algún editor mágico me descubra o iba a publicar online y lograr que la gente pueda leerme?, googlié “donde publicar online” y el primer resultado fue “Wattpad”, un micromundo para escritores adolescentes. No lo dudé y publiqué mi primer novela completa. En el transcurso de un año la novela ganó el premio Watty a la novela más leída, con más de 4 millones de lectores, fans empezaron a tatuarse el símbolo de la novela y hacer remakes del booktrailer, recibí decoraciones, cuadros e infinitos mensajes emotivos. Mientras tanto, continué escribiendo y sumé otras tres novelas a la saga “La triada”, que se pueden leer tanto de forma independiente como en conjunto.- Una vez más: ni una cosa, ni la otra. De pronto tenía 125.000 seguidores en wattpad, pero la novela seguía sin publicarse en papel, así que mis fans crearon una campaña spam en las redes sociales demandándole a un pequeño editor que publique mis novelas, pero ellos no quisieron. La feria del libro estaba terminando en Buenos Aires, corrí y le entregué a un editor de Penguin Random House un dvd con el booktrailer, una sinopsis y una nota. Meses más tarde recibí un mensaje de Penguin diciendo que querían publicar la saga completa. Ella era la editora. Ese mismo año y a mis veintiuno, me recibí cómo directora de cine en la Universidad de Cine de Buenos Aires. Gané el premio centenario Cortázar, firmé y expuse en la charla Outsiders y Las mujeres en la literatura juvenil en la Feria del Libro de Buenos y todos mis proyectos de cortometraje fueron seleccionados para ser producidos incluyendo #red y moscas y mosquitos. Éste último formó parte en la Selección Oficial de la Universidad del Cine 2016 y se estrenó en la selección vanguardia del Festival Internacional de la Habana ese mismo año. También formó parte en la Selección Oficial IPE del festival Al Este de Perú en el 2017. Escribí dos novelas mas. Cuando me recibí, entendí que el viaje ahora tenía que ser físico: había algo de la incomodidad y de salir de la zona de confort que hacía crecer mi necesidad de autoexpresión creativa. Gracias al  gran esfuerzo de mis padres, pude estudiar screenwriting y filmmaking en nueva york. Me involucré vincularmente con gente de todo el mundo, pasé el invierno más frío de mi vida, y gocé y habité el presente cómo nunca la había habitado. Allí filmé The Meaning Of Life Lies In The Pathetic Intention Of Trying To Capture  A Moment, y luego, Oreka en el 2019; ambos distribuidos por Shorts TV. Al volver dicté talleres y asesoramientos literarios en Buenos Aires, hice cursos de redacción, edición literaria y corrección. Empecé a estudiar astrología y mi percepción se volvió a expandir. A mis veinticuatro decidí mudarme a Barcelona sin haberla conocido antes. Fui jurado joven en el festival de san Sebastián y en el 2021 el proyecto de serie I Wonder How It Ends, quedó seleccionado por el sello Disssident. En el 2022 empecé a estudiar astrología mientras escribo nuevas ediciones de mis libros, novelas y guiones. Una de ellas es un thriller de terror psicológico, fantástico y feminista.";
 
